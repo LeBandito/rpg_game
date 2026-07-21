213b41727bf1bfc86b0d89031655e1c6ffdce33c
LeBandito <polslonika@gmail.com>
2026-07-21 15:00:25 +0300

    Первый коммит: структура RPG проекта

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Just Example!" << std::endl;
+    int a;
+    std::cout << "Enter a:\t";
+    std::cin >> a;
+    
+    std::cout << "a is\t" << a << std::endl;
+
+    return 0;
+}
